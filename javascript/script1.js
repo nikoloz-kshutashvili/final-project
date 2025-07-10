cf24066cd8985314ef206aba5e989e9253fcc50c
kshutashvili2008 <nikoloz.qshutashvilite05@geolab.edu.ge>
2025-07-10 18:30:08 +0400

    commit1
    i cant belive it took me this long

---
+++ javascript/script1.js
@@ -0,0 +1,100 @@
+// First image and sound
+const image = document.getElementById('img1');
+const sound = document.getElementById('hoverSound1');
+
+image.addEventListener('mouseenter', () => {
+    sound.currentTime = 0; // Restart sound if you hover multiple times
+    sound.play();
+});
+
+// Second image and sound
+const image1 = document.getElementById('img2');
+const sound1 = document.getElementById('hoverSound2');
+
+image1.addEventListener('mouseenter', () => {  // Correct variable here
+    sound1.currentTime = 0; // Restart sound if you hover multiple times
+    sound1.play();
+});
+
+const image2 = document.getElementById('img3');
+const sound2 = document.getElementById('hoverSound3');
+
+image2.addEventListener('mouseenter', () => {  // Correct variable here
+    sound2.currentTime = 0; // Restart sound if you hover multiple times
+    sound2.play();
+});
+
+const image3 = document.getElementById('img4');
+const sound3 = document.getElementById('hoverSound4');
+
+image3.addEventListener('mouseenter', () => {  // Correct variable here
+    sound3.currentTime = 0; // Restart sound if you hover multiple times
+    sound3.play();
+});
+
+
+const image4 = document.getElementById('img5');
+const sound4 = document.getElementById('hoverSound5');
+
+image4.addEventListener('mouseenter', () => {  // Correct variable here
+    sound4.currentTime = 0; // Restart sound if you hover multiple times
+    sound4.play();
+});
+
+const image5 = document.getElementById('img6');
+const sound5 = document.getElementById('hoverSound6');
+
+image5.addEventListener('mouseenter', () => {  // Correct variable here
+    sound5.currentTime = 0; // Restart sound if you hover multiple times
+    sound5.play();
+});
+
+const image6 = document.getElementById('img7');
+const sound6 = document.getElementById('hoverSound7');
+
+image6.addEventListener('mouseenter', () => {  // Correct variable here
+    sound6.currentTime = 0; // Restart sound if you hover multiple times
+    sound6.play();
+});
+
+
+
+const image9 = document.getElementById('img9');
+const sound9 = document.getElementById('hoverSound9');
+
+image9.addEventListener('mouseenter', () => {  // Correct variable here
+    sound9.currentTime = 0; // Restart sound if you hover multiple times
+    sound9.play();
+});
+
+const image10 = document.getElementById('img10');
+const sound10 = document.getElementById('hoverSound10');
+
+image10.addEventListener('mouseenter', () => {  // Correct variable here
+    sound10.currentTime = 0; // Restart sound if you hover multiple times
+    sound10.play();
+});
+
+const image11 = document.getElementById('img11');
+const sound11 = document.getElementById('hoverSound11');
+
+image11.addEventListener('mouseenter', () => {  // Correct variable here
+    sound11.currentTime = 0; // Restart sound if you hover multiple times
+    sound11.play();
+});
+
+const image12 = document.getElementById('img12');
+const sound12 = document.getElementById('hoverSound12');
+
+image12.addEventListener('mouseenter', () => {  // Correct variable here
+    sound12.currentTime = 0; // Restart sound if you hover multiple times
+    sound12.play();
+});
+
+const image13 = document.getElementById('img13');
+const sound13 = document.getElementById('hoverSound13');
+
+image13.addEventListener('mouseenter', () => {  // Correct variable here
+    sound13.currentTime = 0; // Restart sound if you hover multiple times
+    sound13.play();
+});
